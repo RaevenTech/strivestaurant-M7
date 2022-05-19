4a89175af762a774286306aefe13cc4e88d759c9
RaevenTech <100418770+RaevenTech@users.noreply.github.com>
2022-05-19 15:29:37 +0100

    Reservation and reservation from

---
+++ src/types/Reservation.ts
@@ -0,0 +1,10 @@
+export interface Reservation {
+    _id?: string
+    name: string
+    phone: number | string
+    numberOfPeople: number | string
+    smoking: boolean
+    dateTime: string
+    specialRequests: string
+  }
+  
